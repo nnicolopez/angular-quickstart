44173904163a3d44142c480cad8704c93386f3a9
Nico Lopez <nnicolopez@gmail.com>
2017-08-08 15:59:19 -0300

    adding some routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Hero } from './hero';
-import { HeroService } from './hero.service';
-
-
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    providers: [HeroService]
+    template: `
+        <h1>{{title}}</h1>
+        <nav>
+            <a routerLink="/dashboard">Dashboard</a>
+            <a routerLink="/heroes">Heroes</a>
+        </nav>
+        <router-outlet></router-outlet>
+    `
 })
-
-export class AppComponent implements OnInit {
+export class AppComponent {
     title = 'Tour of Heroes';
-    heroes: Hero[];
-    selectedHero: Hero;
-    onSelect(hero: Hero): void {
-        this.selectedHero = hero;
-    }
-    getHeroes(): void {
-        // this.heroes = this.heroService.getHeroes();
-        this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-    }
-    ngOnInit(): void {
-        this.getHeroes();
-    }
-
-    constructor(private heroService: HeroService) { }
 }
-
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,47 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms'; // <-- NgModel lives here
+import { RouterModule }  from '@angular/router';
 
-import { AppComponent }  from './app.component';
+import { AppComponent }        from './app.component';
+import { HeroesComponent }     from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroService }         from './hero.service';
+import { DashboardComponent }  from './dashboard.component';
+
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    FormsModule // <-- import the FormsModule before binding with [(ngModel)]
-  ],
+imports: [
+  BrowserModule,
+  FormsModule, // <-- import the FormsModule before binding with [(ngModel)]
+  RouterModule.forRoot([
+    {
+      path: 'detail/:id',
+      component: HeroDetailComponent
+    },
+    {
+      path: 'heroes',
+      component: HeroesComponent
+    },
+    {
+      path: 'dashboard',
+      component: DashboardComponent
+    },
+    {
+      path: '',
+      redirectTo: '/dashboard',
+      pathMatch: 'full'
+    }
+  ])
+],
   declarations: [
     AppComponent,
-    HeroDetailComponent
+    HeroesComponent,
+    HeroDetailComponent,
+    DashboardComponent
   ],
+  providers: [ HeroService ],
   bootstrap: [ AppComponent ]
 })
+
 export class AppModule { }
